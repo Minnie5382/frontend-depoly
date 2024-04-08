2cb655b0c918cd2739562c74cff28ccb8431fe5b
O.O_ixn <gmltjr000@gmail.com>
2024-04-08 09:33:40 +0900

    init : 라우터 초기 설정 #1

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Main from '../../pages/main/Main';
+import SignIn from '../../pages/signIn/SignIn';
+import SignUp from '../../pages/signUp/SignUp';
 
 const Router = () => {
-  return <div>Router</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='/signin' element={<SignIn />} />
+      <Route path='/signup' element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default Router;
