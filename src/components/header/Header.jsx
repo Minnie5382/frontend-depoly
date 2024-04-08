915121ac6dc42709c58ee9a056edb7151f1a2c3c
Bella <kbee1202@gmail.com>
2024-04-08 17:26:29 +0900

    design: hearder 사이즈 수정 #8

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,48 +6,56 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/logo2.png';
 
 const Header = () => {
+  const TextFieldTheme = {
+    '& .MuiInputBase-root': {
+      borderBottom: '1px solid var(--point-color)',
+      backgroundColor: '#101418',
+      paddingRight: '3px',
+      '& input': {
+        color: '#fff',
+        width: '20vw',
+      },
+    },
+  };
+
   return (
     <div className={style.header}>
-      <img src={logo} alt='logo' className={style.logo} />
+      <img src={logo} alt="logo" className={style.logo} />
       <div className={style.menu}>
-        <Link to='/' className={style.menuLink}>
+        <Link to="/" className={style.menuLink}>
           메인
         </Link>
-        <Link to='/' className={style.menuLink}>
-          채팅
+        <Link to="/" className={style.menuLink}>
+          채팅방
         </Link>
-        <Link to='/' className={style.menuLink}>
+        <Link to="/" className={style.menuLink}>
           게시판
         </Link>
+      </div>
+      <div className={style.rightMenu}>
         <TextField
           hiddenLabel
-          variant='filled'
-          size='small'
-          sx={{
-            '& .MuiInputBase-root': {
-              borderBottom: '1px solid var(--point-color)',
-              '& input': {
-                color: '#fff',
-              },
-            },
-          }}
+          variant="filled"
+          size="small"
+          sx={TextFieldTheme}
           className={style.searchField}
           InputProps={{
             endAdornment: (
-              <InputAdornment position='end'>
+              <button className={style.searchBtn}>
                 <SearchIcon className={style.searchIcon} />
-              </InputAdornment>
+              </button>
             ),
           }}
         />
-      </div>
-      <div>
-        <Link to='/signin' className={style.menuLink}>
-          로그인
-        </Link>
-        <Link to='/signup' className={style.menuLink}>
-          회원가입
-        </Link>
+
+        <div className={style.menu}>
+          <Link to="/signin" className={style.signBtn}>
+            로그인
+          </Link>
+          <Link to="/signup" className={style.signBtn}>
+            회원가입
+          </Link>
+        </div>
       </div>
     </div>
   );
