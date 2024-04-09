4643ba042d8781dc5610d176e385bd9587922f9f
O.O_ixn <gmltjr000@gmail.com>
2024-04-09 18:42:46 +0900

    design : 회원가입 페이지 ui 수정

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,241 +1,86 @@
-import React from 'react';
-import { Button, TextField, Link, Stack } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Button,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  Link,
+} from '@mui/material';
 import style from './SignUp.module.css';
 import logo from '../../assets/logo2.png';
+import MobileSignUpForm from './MobileSignUpForm';
+import KakaoSignUpForm from './KakaoSignUpForm';
 
 const SignUp = () => {
+  const [signUpOption, setSignUpOption] = useState('kakao');
+  const inputTheme = {
+    '& .MuiInputLabel-root': { color: 'var(--text-color)' },
+    '& .Mui-focused': { color: 'var(--point-color)' },
+    '& .MuiInput-underline:after': { borderBottomColor: 'var(--text-color)' },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': { borderColor: 'var(--text-color)' },
+      '&:hover fieldset': { borderColor: 'var(--point-color)' },
+      '&.Mui-focused fieldset': { borderColor: 'var(--point-color)' },
+      '& input': { color: 'var(--text-color)' },
+    },
+  };
+
+  const handleSignUpOptionChange = (event) => {
+    setSignUpOption(event.target.value);
+  };
+
   return (
     <div className={style.pageContainer}>
       <img src={logo} alt='로고' className={style.logo} />
       <div className={style.signUpContainer}>
         <span className={style.title}>회원가입</span>
+        <FormControl component='fieldset'>
+          <FormLabel component='legend' sx={{ color: 'white' }}>
+            가입 방식 선택
+          </FormLabel>
+          <RadioGroup
+            row
+            aria-label='signUpOption'
+            name='signUpOption'
+            value={signUpOption}
+            onChange={handleSignUpOptionChange}
+          >
+            <FormControlLabel
+              value='kakao'
+              control={<Radio sx={{ color: 'white' }} />}
+              label='카카오 계정으로 가입하기'
+            />
+            <FormControlLabel
+              value='mobile'
+              control={<Radio sx={{ color: 'white' }} />}
+              label='모바일로 가입하기'
+            />
+          </RadioGroup>
+        </FormControl>
         <form className={style.form}>
-          <TextField
-            label='이메일'
-            type='email'
-            variant='outlined'
-            fullWidth
-            size='small'
-            required
-            sx={{
-              '& .MuiInputLabel-root': {
-                color: 'var(--text-color)',
-              },
-              '& .Mui-focused': {
-                color: 'var(--point-color)',
-              },
-              '& .MuiInput-underline:after': {
-                borderBottomColor: 'var(--text-color)',
-              },
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderColor: 'var(--text-color)',
-                },
-                '&:hover fieldset': {
-                  borderColor: 'var(--point-color)',
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: 'var(--point-color)',
-                },
-                '& input': {
-                  color: '#fff',
-                },
-              },
-            }}
-          />
-
-          <TextField
-            label='비밀번호'
-            type='password'
-            variant='outlined'
-            size='small'
-            fullWidth
-            required
-            sx={{
-              '& .MuiInputLabel-root': {
-                color: 'var(--text-color)',
-              },
-              '& .Mui-focused': {
-                color: 'var(--point-color)',
-              },
-              '& .MuiInput-underline:after': {
-                borderBottomColor: 'var(--text-color)',
-              },
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderColor: 'var(--text-color)',
-                },
-                '&:hover fieldset': {
-                  borderColor: 'var(--point-color)',
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: 'var(--point-color)',
-                },
-                '& input': {
-                  color: '#fff',
-                },
-              },
-            }}
-          />
-
-          <TextField
-            label='비밀번호 확인'
-            type='password'
-            variant='outlined'
-            size='small'
-            fullWidth
-            required
-            sx={{
-              '& .MuiInputLabel-root': {
-                color: 'var(--text-color)',
-              },
-              '& .Mui-focused': {
-                color: 'var(--point-color)',
-              },
-              '& .MuiInput-underline:after': {
-                borderBottomColor: 'var(--text-color)',
-              },
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderColor: 'var(--text-color)',
-                },
-                '&:hover fieldset': {
-                  borderColor: 'var(--point-color)',
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: 'var(--point-color)',
-                },
-                '& input': {
-                  color: '#fff',
-                },
-              },
-            }}
-          />
-
-          <TextField
-            label='이름'
-            type='text'
-            variant='outlined'
-            fullWidth
-            size='small'
-            required
-            sx={{
-              '& .MuiInputLabel-root': {
-                color: 'var(--text-color)',
-              },
-              '& .Mui-focused': {
-                color: 'var(--point-color)',
-              },
-              '& .MuiInput-underline:after': {
-                borderBottomColor: 'var(--text-color)',
-              },
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderColor: 'var(--text-color)',
-                },
-                '&:hover fieldset': {
-                  borderColor: 'var(--point-color)',
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: 'var(--point-color)',
-                },
-                '& input': {
-                  color: '#fff',
-                },
-              },
-            }}
-          />
-
-          <Stack direction='row' spacing={1}>
-            <TextField
-              label='휴대폰 번호'
-              type='text'
-              variant='outlined'
-              fullWidth
-              required
-              size='small'
-              sx={{
-                '& .MuiInputLabel-root': {
-                  color: 'var(--text-color)',
-                },
-                '& .Mui-focused': {
-                  color: 'var(--point-color)',
-                },
-                '& .MuiInput-underline:after': {
-                  borderBottomColor: 'var(--text-color)',
-                },
-                '& .MuiOutlinedInput-root': {
-                  '& fieldset': {
-                    borderColor: 'var(--text-color)',
-                  },
-                  '&:hover fieldset': {
-                    borderColor: 'var(--point-color)',
-                  },
-                  '&.Mui-focused fieldset': {
-                    borderColor: 'var(--point-color)',
-                  },
-                  '& input': {
-                    color: '#fff',
-                  },
-                },
-              }}
-            />
-            <Button variant='contained'>인증</Button>
-          </Stack>
-
-          <Stack direction='row' spacing={1}>
-            <TextField
-              label='인증번호 입력'
-              type='text'
-              variant='outlined'
-              fullWidth
-              size='small'
-              required
-              sx={{
-                '& .MuiInputLabel-root': {
-                  color: 'var(--text-color)',
-                },
-                '& .Mui-focused': {
-                  color: 'var(--point-color)',
-                },
-                '& .MuiInput-underline:after': {
-                  borderBottomColor: 'var(--text-color)',
-                },
-                '& .MuiOutlinedInput-root': {
-                  '& fieldset': {
-                    borderColor: 'var(--text-color)',
-                  },
-                  '&:hover fieldset': {
-                    borderColor: 'var(--point-color)',
-                  },
-                  '&.Mui-focused fieldset': {
-                    borderColor: 'var(--point-color)',
-                  },
-                  '& input': {
-                    color: '#fff',
-                  },
-                },
-              }}
-            />
-            <Button variant='contained'>확인</Button>
-          </Stack>
-
+          {signUpOption === 'mobile' && (
+            <MobileSignUpForm inputTheme={inputTheme} />
+          )}
+          {signUpOption === 'kakao' && (
+            <KakaoSignUpForm inputTheme={inputTheme} />
+          )}
           <Button
             variant='contained'
             fullWidth
             type='submit'
-            sx={{ height: '30px' }}
+            sx={{ height: '40px' }}
           >
             회원가입
           </Button>
-
-          <div className={style.signInPrompt}>
-            <span>이미 아이디가 있으신가요?&nbsp;&nbsp;</span>
-            <Link href='/signin' underline='none'>
-              로그인 하러가기
-            </Link>
-          </div>
         </form>
+        <div className={style.signInPrompt}>
+          <span>이미 아이디가 있으신가요?&nbsp;</span>
+          <Link href='/signin' underline='none'>
+            로그인 하러가기
+          </Link>
+        </div>
       </div>
     </div>
   );
