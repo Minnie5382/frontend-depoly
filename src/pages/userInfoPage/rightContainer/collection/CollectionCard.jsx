d41d3467cc788507c3c97dc92e94a21c5bce08aa
O.O_ixn <gmltjr000@gmail.com>
2024-04-28 17:41:51 +0900

    feat : 콜렉션 탭 수정 #67

--- src/pages/userInfoPage/rightContainer/collection/CollectionCard.jsx
+++ src/pages/userInfoPage/rightContainer/collection/CollectionCard.jsx
@@ -3,16 +3,22 @@ import style from '../../UserInfoPage.module.css';
 import EditDeleteButtons from '../../../../components/button/EditDeleteButtons';
 import LikeButton from '../../../../components/button/LikeButton';
 
-const CollectionCard = ({ content, star, likeNumber, createdAt, reviewId }) => {
+const CollectionCard = ({
+  content,
+  userScore,
+  likeNumber,
+  movieTitle,
+  reviewId,
+}) => {
   return (
     <div className={style.collection}>
       <div className={style.topSection}>
         <div className={style.info}>
           <div className={style.infoBox}>
-            <div className={style.time}>{createdAt}</div>
+            <div className={style.time}>{movieTitle}</div>
           </div>
         </div>
-        <div className={style.stars}>★ {star}</div>
+        <div className={style.stars}>★ {userScore}</div>
       </div>
       <hr />
       <div className={style.content}>{content}</div>
