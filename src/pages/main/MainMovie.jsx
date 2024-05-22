bbd51fcb9ada68f71edd9a7da33c622918dcc66f
O.O_ixn <gmltjr000@gmail.com>
2024-05-22 12:42:16 +0900

    fix : 오류 수정 #130

--- src/pages/main/MainMovie.jsx
+++ src/pages/main/MainMovie.jsx
@@ -27,8 +27,8 @@ const MainMovie = ({
         <></>
       ) : (
         <span className={style.stars}>
-          {levelAvgScore !== 0 && <div>⭐️ {levelAvgScore}</div>}
-          {cinephileAvgScore !== 0 && <div>⭐️ {cinephileAvgScore}</div>}
+          {levelAvgScore === 0 && <div>⭐️ {levelAvgScore}</div>}
+          {cinephileAvgScore === 0 && <div>⭐️ {cinephileAvgScore}</div>}
         </span>
       )}
       <div>{releaseDate}</div>
